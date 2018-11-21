8d8cd2286c4fb16b25a90bd239a71c55c48158b5
Yann PIQUET <yann.piquet@inference-operations.com>
2018-11-21 11:40:50 +0100

    Add random to person generator

--- manager/generators/person.py
+++ manager/generators/person.py
@@ -1,3 +1,4 @@
+from random import randint
 from faker import Faker
 from django.conf import settings
 
@@ -5,7 +6,7 @@ from django.conf import settings
 def person_generator(t, rank):
     fake = Faker(settings.LANGUAGE_CODE)
     profile = fake.simple_profile()
-    data = rank % 4
+    data = randint(0, 3)
     if data == 0:
         return profile['name']
     elif data == 1:
